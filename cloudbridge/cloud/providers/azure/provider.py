47c60ce6047abf95fbb8c3790e1e2e87c6a09b8c
Nuwan Goonasekera <nuwan.ag@gmail.com>
2015-12-01 23:10:12 +1100

    Add service provider stubs.

--- cloudbridge/cloud/providers/azure/provider.py
+++ cloudbridge/cloud/providers/azure/provider.py
@@ -10,3 +10,22 @@ class AzureCloudProvider(BaseCloudProvider):
 
     def __init__(self, config):
         super(AzureCloudProvider, self).__init__(config)
+
+    @property
+    def compute(self):
+        raise NotImplementedError(
+            "AzureCloudProvider does not implement this service")
+
+    @property
+    def security(self):
+        return self._security
+
+    @property
+    def block_store(self):
+        raise NotImplementedError(
+            "AzureCloudProvider does not implement this service")
+
+    @property
+    def object_store(self):
+        raise NotImplementedError(
+            "AzureCloudProvider does not implement this service")
